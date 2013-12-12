e3f4a68d4bb86b4816986326e0c92fc4f79f2c20
Leon Searl <searl@ittc.ku.edu>
2013-12-12 17:48:10 -0600

    mods for the PXE server

--- INSTALL/smallDesktopCloud/router/pxe.sh
+++ INSTALL/smallDesktopCloud/router/pxe.sh
@@ -26,39 +26,14 @@ yum install syslinux tftp tftp-server -y || exit 1
 
 SYSLINUX_DIR=/usr/share/syslinux
 TFTPBOOT_DIR=/var/lib/tftpboot
+WWW_DIR=/var/www/html
 
-CENTOS65_X8664_IMAGES=${TFTPBOOT_DIR}/images/CentOS6.5/x86_64
+echo "# Extract the TFTPBOOT files."
+cd ${TFTPBOOT_DIR}
+tar -zxf ${SHELLDIR}/tftpboot.tgz
 
-cp ${SYSLINUX_DIR}/menu.c32 ${TFTPBOOT_DIR}
-cp ${SYSLINUX_DIR}/pxelinux.0 ${TFTPBOOT_DIR}
-mkdir -p ${CENTOS65_X8664_IMAGES}
-
-cd ${CENTOS65_X8664_IMAGES}
-
-WGETFILE=http://kickstart.ittc.ku.edu/mirror/redhat/el/6.5/server/os/x86_64/images/pxeboot/initrd.img
-wget -v ${WGETFILE} || exit 1
-
-WGETFILE=http://kickstart.ittc.ku.edu/mirror/redhat/el/6.5/server/os/x86_64/images/pxeboot/vmlinuz
-wget ${WGETFILE} || exit 1
-
-echo "second wget"
-
-mkdir -p ${TFTPBOOT_DIR}/pxelinux.cfg
-
-cat > ${TFTPBOOT_DIR}/pxelinux.cfg/default <<EOF
-timeout 100
-default menu.c32
- 
-menu title ########## Armored Boot Menu ##########
-label 1
-   menu label ^1) Install CentOS 6.5
-   kernel ${CENTOS65_X8664_IMAGES}/vmlinuz
-   append initrd=${CENTOS65_X8664_IMAGES}/initrd.img devfs=nomount ks=http://${CLOUD_EXT_ROUTER_HOSTNAME}.${CLOUD_EXT_DOMAIN}/centos65.ks
- 
-label 2
-   menu label ^2) Boot from local drive
-   localboot
-EOF
+#cp ${SYSLINUX_DIR}/menu.c32 ${TFTPBOOT_DIR}
+#cp ${SYSLINUX_DIR}/pxelinux.0 ${TFTPBOOT_DIR}
 
 echo "# Enablethe TFTP service"
 sed -i -e "/disable/s/=.*/= no/" /etc/xinetd.d/tftp
@@ -71,11 +46,13 @@ echo "# Setup the http service."
 
 yum -y install httpd || exit 1 
 
+echo "# copy the kickstart files to the http service."
+cd ${WWW_DIR}
+tar -zxf ${SHELLDIR}/www_html.tgz
+
 systemctl enable xinetd.service
 systemctl restart xinetd.service
 
 systemctl enable httpd.service
 systemctl restart httpd.service
-
-echo "Put web files at /var/www/html"
 
