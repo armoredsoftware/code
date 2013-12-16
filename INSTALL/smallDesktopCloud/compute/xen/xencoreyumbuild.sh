38f2133b8f09e9e5f6888c935672f842f0486a83
armored <armored@compute2.ext.armored>
2013-12-16 11:03:25 -0600

    Now allow the xenxerver rpms to be installed in to our armored yum database.

---
+++ INSTALL/smallDesktopCloud/compute/xen/xencoreyumbuild.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Build the yum repository for installing the xenserver-core.
+# This script should be run on the 'router' host. 
+# It requireds the xenserver-core-latest.tgz tarball.
+
+# We need 'mock' to do the install.
+
+TOP_LEVEL=${TOP_LEVEL:-../..}
+UTIL_DIR=${UTIL_DIR:-${TOP_LEVEL}/util}
+{ . ${UTIL_DIR}/fns ; } || { echo "!!! Failed to source file ${UTIL_DIR}/fns." ; exit 1; }
+
+BUILDDIR=/xenserver-core-build
+REPO_DIR=/var/www/html/armoredrepo
+REPO_CENTOS65=${REPO_DIR}/CentOS6.5
+
+
+
+# We have to be the root user.
+check_root_user
+
+# Puppet must be disabled to prevent it from over writing the passwd and group
+# files since the mock user will be added and 
+echo "# !!!! Shutdown puppet."
+which systemctl 2> /dev/null
+if [ $? -eq 0 ] ; then
+  systemctl stop puppetagent.service
+  systemctl disable puppetagent.service
+else
+  service puppet status
+  if [ $? -eq 0 ] ; then
+    service puppet stop
+    chkconfig puppet off
+  fi
+fi
+
+
+echo "# Creating directory /xenserver_core for tar ball"
+if [ ! -f ${BUILDDIR} ] ; then
+  mkdir ${BUILDDIR}
+  chmod a+xwr ${BUILDDIR}
+fi
+
+ We need to get the rpm tarball from somewhere.
+if [ $# -eq 1 ] ; then
+  # Remove the old dir
+  if [ -f ${BUILDDIR}/xenserver-core ] ; then
+    rm -rf ${BUILDDIR}/xenserver-core
+  fi
+  # Unpack the tarball.
+  tar -C ${BUILDDIR} -zxf $1 
+else
+  if [ -f ${BUILDDIR}/xenserver-core-latest.tgz ] ; then
+    if [ -f ${BUILDDIR}/xenserver-core ] ; then
+      rm -rf ${BUILDDIR}/xenserver-core
+    fi
+    cd ${BUILDDIR}
+    tar -zxf ${BUILDDIR}/xenserver-core-latest.tgz
+  else
+    echo "!!!! Could not find xenserver-core RPM tarball."
+    echo "Give it as the first argument to this script."
+    exit 1
+  fi
+fi
+
+cd ${BUILDDIR}/xenserver-core/scripts/rpm/
+cp xen-c6-tweaked.repo ${REPO_CENTOS65}/xen-c6-tweaked.repo
+
+cd ${BUILDDIR}/xenserver-core/RPMS/
+cp -r . ${REPO_CENTOS65}
+
