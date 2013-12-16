fd724b67ba9f89e61d352694e559e5e946f94ae3
armored <armored@compute2.ext.armored>
2013-12-16 13:07:59 -0600

    fixed repo issues

--- INSTALL/smallDesktopCloud/compute/xen/xencoreinstall.sh
+++ INSTALL/smallDesktopCloud/compute/xen/xencoreinstall.sh
@@ -72,6 +72,18 @@ fi
 #cd ${BUILDDIR}/xenserver-core
 #. scripts/rpm/install.sh
 
+echo "Get the repo config files."
+
+cd /tmp
+wget http://router.ext.armored/armoredrepo/CentOS6.5/xen-c6-tweaked.repo
+wget http://router.ext.armored/armoredrepo/CentOS6.5/epel.repo
+wget http://router.ext.armored/armoredrepo/CentOS6.5/ArmoredConfig.repo
+
+install -m 0644 xen-c6-tweaked.repo /etc/yum.repos.d/xen-c6-tweaked.repo
+install -m 0644 epel.repo /etc/yum.repos.d/epel.repo
+install -m 0644 ArmoredConfig.repo /etc/yum.repos.d/ArmoredConfig.repo
+
+
 yum install -y xenserver-core
 
 echo "#Setting up ld.so for libvhd.so.0"
