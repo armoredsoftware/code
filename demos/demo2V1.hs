6e982b9c3e2012cfe6540e2f061dacc147d70ca5
Paul I Kline <pauliankline@gmail.com>
2014-06-10 16:30:59 -0500

    Second commit, still don't look. Just testing git.

--- demos/demo2V1.hs
+++ demos/demo2V1.hs
@@ -1,13 +1,16 @@
 module Demo2V1 where
 
-import System.Ramdom
+import System.Random
 
 {--
 generateNonce :: Int 
 generateNonce = 
---}
+
 genereateNonceHelper :: IO Int
-generateNonceHelper = getStdRandom((minBound Int), (maxBound Int))
+generateNonceHelper = getStdRandom((-9000), (9000))
+--}
+rollDice :: IO Int
+rollDice = getStdRandom (randomR ((minBound ::Int),maxBound :: Int))
 
 dosomethin :: Int -> Int
 dosomethin x = 2*x
