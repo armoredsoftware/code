effabe9a208cfd0f6d6d4234e9d37face4bb7250
Paul Kline <pauliankline@gmail.com>
2014-06-18 15:37:52 -0500

    Working language parser in SimpleParser2.hs. At least.. the factorial function works. No further testing has been done.

--- demos/PaulPractice/SimpleParser2.hs
+++ demos/PaulPractice/SimpleParser2.hs
@@ -1,6 +1,5 @@
 --module SimpleParser where
 
-
 import Prelude
 import Data.List
 import Data.String
@@ -12,7 +11,6 @@ import System.Environment
          | c ;; c
          | WHILE b DO c END
          | IFB b THEN c ELSE c FI
- 
  
 For example, here's the factorial function in Imp.
  
@@ -66,7 +64,6 @@ eval2 (Not x) v = not (eval2 x v)
 eval2 (x `And` y) v = and [(eval2 x v), (eval2 y v)]
 
 
-
 main :: IO ()
 main = do
        [f] <- getArgs
@@ -79,27 +76,10 @@ main = do
 --       contents <- sequence $ fmap readFile xs   
 --       mapM_ putStr (contents) 
 
-testRead :: String -> Int
-testRead x = eval (read x:: A) []
-
 --Step 1: Get the program words
 type Code = String
 parseToWords:: String -> [String]
 parseToWords s = words s  
-
-
---separate by ;;'s
-separateIntoCommands :: [String]-> [String] -> [[String]]
-separateIntoCommands [] xs = [xs ++ []]
-separateIntoCommands (x : xs) cur | (isSuffixOf ";;" x) = (cur ++ [x]) : (separateIntoCommands xs [])
---now get 'parts'
-                                  | otherwise = separateIntoCommands xs (cur ++ [x])
----createCommands :: [[String]] -> 
-
-
-
-test :: String -> Bool
-test x = isSuffixOf ";;" x
 
 data C = Code C C
          | SKIP
@@ -112,7 +92,11 @@ instance Show C where
   show (Loop x xs) = "Loop: " ++ x ++ show xs
   show (Cond x y z) = "Cond: " ++ x ++ "If true: " ++ show y ++ "Else: " ++ show z
   show (Assign x y) = "Assign: " ++ x ++ " to " ++ y
-  
+
+
+--no error checking, your code must be perfect!
+  --converts a program to a list a variables that hold the results of running the program. Variables can only be
+--one character capitals that hold an int.
 ultimateEvalH :: C ->Variables -> Variables
 ultimateEvalH (Code x y) vars = ultimateEvalH y (ultimateEvalH x vars)
 ultimateEvalH SKIP vars = vars
@@ -123,8 +107,8 @@ ultimateEvalH (Cond x y z) vars | (eval2 (read x:: B) vars) = ultimateEvalH (con
 ultimateEvalH (Assign x y) vars = case (lookup x vars) of
                           Nothing -> (x, (eval (read y:: A) vars)) : vars --if not present, add it!
                           Just a ->((x, (eval (read y:: A) vars)) : (filter (\p -> ((fst p) /=x) ) vars))  --if it's present, replace it!
---ultimateEvalH _ vars = vars
 
+--Converts the words of the program into a single Command
 constructTree :: [String] -> C
 constructTree [] = SKIP
 constructTree (x : xs) | x=="WHILE" = Code (Loop (parseB xs "") (getBody xs ("DO", "END"))) (constructTree (ignoreTillBlockEnd xs ("DO", "END")))
@@ -132,24 +116,28 @@ constructTree (x : xs) | x=="WHILE" = Code (Loop (parseB xs "") (getBody xs ("DO
                        | length x == 1 && ((head x) `elem` ['A' .. 'Z']) = Code (Assign x (getA xs "")) (constructTree (ignoreAssignment xs))
                        | otherwise = SKIP
 
+
+--gets the BExpression as a string. In this language, BExpressions are only found preceding a "DO" or a "THEN"
 type BExpression= String
 parseB :: [String]->String-> BExpression
 parse [] r = r
 parseB (x : xs) s | x == "DO" || x == "THEN" = s
                   | otherwise = parseB xs (s ++" " ++ x)
 
+--gets the AExpression as a string. In this language, A expressions are only found in assignments.
 getA :: [String] -> String -> String
 getA [] bod = bod
 getA (x:xs) bod | isSuffixOf ";;" x = bod ++" "++ (takeWhile (/=';') x)
                 | x =="::=" = getA xs bod
                 | otherwise = getA xs (bod ++ x)
 
+--gets the rest of the program, ignoring the first assigment traversed.
 ignoreAssignment :: [String] -> [String]
 ignoreAssignment [] = []
 ignoreAssignment (x:xs) | isSuffixOf ";;" x = xs
                         | otherwise = ignoreAssignment xs
 
---code repitition, but I'm not sure how to do it another way yet
+--ignores the code in the kind of block specified by (String, String) and returns all code after 
 ignoreTillBlockEnd:: [String] -> (String,String) -> [String]
 ignoreTillBockEnd [] _ = []
 ignoreTillBlockEnd ls (start,stop) = ignoreTillBlockEndHelper ls (start,stop) 0
@@ -160,8 +148,9 @@ ignoreTillBlockEndHelper (x : xs) (start,stop) i
                                         | x==start = ignoreTillBlockEndHelper xs (start,stop) (i+1)
                                         | x==stop && i>0 = ignoreTillBlockEndHelper xs (start,stop) (i-1)
                                         | x==stop && i==0 = xs
-                                        | otherwise = ignoreTillBlockEndHelper xs (start,stop) i 
-
+                                        | otherwise = ignoreTillBlockEndHelper xs (start,stop) i
+                                                      
+--does the compliment of ignoreTillBlockEnd. Returns the code in the specified body tag (String,String)
 getBody :: [String] -> (String, String) -> [String]
 getBody [] _ = []
 getBody (x:xs) (start,stop)  | x ==start = getBodyHelper xs [] (start,stop) 0
