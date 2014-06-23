337920c75c5971a48ee666ec3fa80fea1ac3f447
Paul Kline <pauliankline@gmail.com>
2014-06-23 12:25:00 -0500

    Appears to work so long as no variable naming conflicts. Working on that part now.

--- demos/PaulPractice/lambdaInterp.hs
+++ demos/PaulPractice/lambdaInterp.hs
@@ -1,15 +1,32 @@
-type Arg = Char
-type Body = [Char]
+data T = Var Char |
+         Abst T T |
+         App T T deriving (Eq, Show)
 
 
-data T = V Char |
-         L Arg T |
-         App T T
-{--
+
 eval :: T -> T
-eval (App (L x t) (V y)) = eval 
-   --}  
+eval (App (Abst x y) z) = eval (sub y x z)
+eval (Abst x (App a b)) = Abst x (eval (App (eval a) (eval b)))
+eval (App x y) = eval (App (eval x) (eval y))
+eval x = x
 
-replace ::(Eq a)=> a -> a -> [a] -> [a]
-replace _ _ [] = []
-replace x y xs = map (\v-> if v==x then y else v) xs
+
+-- in T replace T with T yields T
+sub :: T -> T -> T -> T
+sub (Var x) (Var y) z = if x ==y then z else Var x
+sub (Abst (Var a) x) (Var y) z = Abst (Var a) (sub x (Var y) z)
+sub (App a b) (Var y) z = App (sub a (Var y) z) (sub b (Var y) z)
+
+
+
+tru =  Abst (Var 't') (Abst (Var 'f') (Var 't'))
+fls = Abst (Var 't') (Abst (Var 'f') (Var 'f'))
+and' = Abst (Var 'b') (Abst (Var 'c') (App (App (Var 'b') (Var 'c')) fls))
+
+
+
+{-test data
+tru == eval (App( App and' tru) fls)
+tru == eval (App( App and' tru) tru)
+tru == eval (App( App and' fls) tru)
+-}
