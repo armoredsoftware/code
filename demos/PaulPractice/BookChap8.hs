d5a2130f0e350c995277f6851bd91f01763aae22
Paul Kline <pauliankline@gmail.com>
2014-06-24 12:53:19 -0500

    This type checking version contains error types. Let me know what you think.

--- demos/PaulPractice/BookChap8.hs
+++ demos/PaulPractice/BookChap8.hs
@@ -31,30 +31,49 @@ getType x = ERROR
 
 
 
-         
 eval :: T -> T
-
---eval (If TRUE y _) = eval y
-eval (If x y z)
-            | (getType x) /= ERROR =
-                                   | x == TRUE = eval y
-                                   | otherwise = eval z
-            | otherwise = BOOL_EXPECTED_ERROR
-eval (If x y z) = If (eval x) y z
-
-eval (Succ TRUE) = NUM_EXPECTED_ERROR
-eval (Succ FALSE) = NUM_EXPECTED_ERROR
-eval (Succ x) = Succ (eval x)
-
-eval (Pred ZERO) = ZERO
-eval (Pred (Succ x)) = eval x
-eval (Pred TRUE) = NUM_EXPECTED_ERROR
-eval (Pred FALSE) = NUM_EXPECTED_ERROR
-eval (Pred x) = Pred (eval x)
-
-eval (IsZero ZERO) = TRUE
-eval (IsZero (Succ x)) = FALSE
-eval (IsZero TRUE) = NUM_EXPECTED_ERROR
-eval (IsZero FALSE) = NUM_EXPECTED_ERROR
-eval (IsZero x) = IsZero (eval x)
+eval (If x y z) | (getType x) ==BOOL = case (x) of
+                                          (TRUE)  -> eval y
+                                          (FALSE) -> eval z
+                                          (_)     -> eval (If (eval x) y z)
+                | otherwise = BOOL_EXPECTED_ERROR
+eval (Succ x)
+            | (getType x) == NAT = case (x) of
+                                 (ZERO)  -> ZERO
+                                 (Val v) -> Val (v+1)
+                                 (_)     -> eval (Succ (eval x))
+            | otherwise = NUM_EXPECTED_ERROR
+eval (Pred x)
+            | (getType x) ==NAT = case (x) of
+                                        (ZERO)  -> Val 1
+                                        (Val 1) -> ZERO
+                                        (Val v) -> Val (v-1)
+                                        (_)     -> eval (Pred (eval x))
+            | otherwise = NUM_EXPECTED_ERROR
+eval (IsZero x)
+              | (getType x) == NAT = case (x) of
+                                             (ZERO) -> TRUE
+                                             (Val v)-> FALSE
+                                             (_)    -> eval (IsZero (eval x))
+              | otherwise = NUM_EXPECTED_ERROR
 eval (x) = x
+
+
+{-- Test data
+*Main> eval (If (If TRUE (Val 3) (Val 4)) (Val 9) (Val 20))
+BOOL_EXPECTED_ERROR
+*Main> eval (If (If TRUE (FALSE) (TRUE)) (Val 9) (Val 20))
+Val 20
+
+*Main> eval (Pred (Val 1))
+ZERO
+*Main> getType (If TRUE TRUE TRUE)
+BOOL
+*Main> getType (If (Val 3) TRUE TRUE)
+ERROR
+*Main> getType (If (TRUE) (Val 3)  TRUE)
+ERROR
+*Main> getType (If (TRUE) (Val 3)  (Val 4))
+NAT
+
+--}
