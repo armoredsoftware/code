447407aca81ee13319cabaeca7325fe3404c4df7
Evan Austin <ecaustin@ittc.ku.edu>
2014-06-24 14:29:53 -0500

    Revised simply typed lambda interpreter.

--- demos/PaulPractice/BookChap8.hs
+++ demos/PaulPractice/BookChap8.hs
@@ -1,63 +1,64 @@
-data T = TRUE |
-         FALSE |
-         If T T T |
-         ZERO |
-         Val Int |
-         Pred T |
-         IsZero T |
-         Succ T |
-         NUM_EXPECTED_ERROR |
-         BOOL_EXPECTED_ERROR deriving Show
+module BookChap8 where
+
+data T = TRUE 
+       | FALSE 
+       | If T T T
+       | ZERO
+       | Pred T
+       | IsZero T
+       | Succ T deriving (Eq, Show)
 
 
-data TYPE = BOOL |
-             NAT |
-             ERROR deriving (Eq, Show)
+data TYPE = BOOL
+          | NAT deriving (Eq, Show)
 
-getType :: T -> TYPE
-getType TRUE = BOOL
-getType FALSE = BOOL
-getType (If x y z) = if getType x == BOOL && getType y == getType z then
-                       getType y
-                     else
-                       ERROR
-
-getType (Pred t) = if getType t == NAT then NAT else ERROR
-getType (Succ t) = if getType t == NAT then NAT else ERROR
-getType (IsZero t) = if getType t == NAT then BOOL else ERROR
+getType :: T -> Either String TYPE
+getType TRUE = Right BOOL
+getType FALSE = Right BOOL
+getType (If x y z) =
+    do x' <- getType x
+       y' <- getType y
+       z' <- getType z
+       if x' == BOOL && y' == z'
+          then return y'
+          else Left "Bad Type: conditional"
+getType (Pred t) 
+    do t' <- getType t
+       if t' == NAT
+          then return NAT
+          else Left "Bad Type: pred."
+getType (Succ t) 
+    do t' <- getType t
+       if t' == NAT
+          then return NAT
+          else Left "Bad Type: succ."
+getType (IsZero t) 
+    do t' <- getType t
+       if t' == NAT
+          then return BOOL
+          else Left "Bad Type: zero?."
 getType ZERO = NAT
-getType Val{} = NAT
-getType x = ERROR
-
 
 
 eval :: T -> T
-eval (If x y z) | getType x ==BOOL = case x of
-                                          (TRUE)  -> eval y
-                                          (FALSE) -> eval z
-                                          (_)     -> eval (If (eval x) y z)
-                | otherwise = BOOL_EXPECTED_ERROR
-eval (Succ x)
-            | getType x == NAT = case x of
-                                 (ZERO)  -> ZERO
-                                 (Val v) -> Val (v+1)
-                                 (_)     -> eval (Succ (eval x))
-            | otherwise = NUM_EXPECTED_ERROR
-eval (Pred x)
-            | getType x ==NAT = case x of
-                                        (ZERO)  -> Val 1
-                                        (Val 1) -> ZERO
-                                        (Val v) -> Val (v-1)
-                                        (_)     -> eval (Pred (eval x))
-            | otherwise = NUM_EXPECTED_ERROR
-eval (IsZero x)
-              | getType x == NAT = case x of
-                                             (ZERO) -> TRUE
-                                             (Val v)-> FALSE
-                                             (_)    -> eval (IsZero (eval x))
-              | otherwise = NUM_EXPECTED_ERROR
-eval (x) = x
+eval (If TRUE y _) =  eval y 
+eval (If FALSE _ z) = eval z
+eval (If x y z) = eval $ If (eval x) y z
+eval (Succ ZERO) = Val 1
+eval (Succ x) = 
+    let (Val x') = eval x in
+      Val $ x' + 1
+eval (Pred ZERO) = ZERO
+eval (IsZero ZERO) = TRUE
+eval (IsZero Succ{}) = FALSE
+eval (IsZero x) = IsZero $ eval x
+eval x = x
 
+run :: T -> T
+run tm =
+    case getType tm of
+      Right _ -> eval tm
+      Left str -> error str
 
 {-- Test data
 *Main> eval (If (If TRUE (Val 3) (Val 4)) (Val 9) (Val 20))
