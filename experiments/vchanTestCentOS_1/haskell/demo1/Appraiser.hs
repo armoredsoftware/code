5edaa61ba0a66b4853247ab8ab5993eea5cc35a3
Evan Austin <ecaustin@ittc.ku.edu>
2014-07-01 11:42:07 -0500

    Ascribing a type to fix an error.

--- experiments/vchanTestCentOS_1/haskell/demo1/Appraiser.hs
+++ experiments/vchanTestCentOS_1/haskell/demo1/Appraiser.hs
@@ -5,7 +5,7 @@ import VChanUtil
 
 import Control.Monad
 import Demo1Utils
-import Crypto.Random.API (cprgCreate)
+import Crypto.Random.API (cprgCreate, SystemRNG)
 import Crypto.Random (createEntropyPool)
 
 import Data.Maybe
