ede2760dbd887ca6b82745764c5d2557054687df
ampetz <armored@compute5.ext.armored>
2014-07-15 14:52:48 -0500

    First changes to Appraiser and cleanup

--- demos/demo2/Appraiser.hs
+++ demos/demo2/Appraiser.hs
@@ -8,6 +8,7 @@ import Crypto.Random
 import Crypto.PubKey.HashDescr
 import Crypto.PubKey.RSA
 import Crypto.PubKey.RSA.PKCS15
+import Crypto.Hash.MD5(hash)
 
 -- utility libraries
 import Control.Exception hiding (evaluate)
@@ -20,7 +21,7 @@ import System.IO.Unsafe (unsafePerformIO)
 import Data.Binary
 
 data Shared = Appraisal Request
-              | Attestation Quote
+              | Attestation Response
               | Result Bool
 
 
@@ -29,14 +30,7 @@ instance Show Shared where
     show (Attestation att) = "Attestation: " ++ (show att)
     show (Result True) = "Appraisal succeeded."
     show (Result False) = "Appraisal failed."
-
--- Primitive types
-type PCR = Word8
-type Mask = Word8
-type Nonce = ByteString
-type Signature = ByteString
-type Request = (Mask, Nonce)
-type Quote = (([PCR], Nonce), Signature)
+    
 
 instance Binary Shared where
   put (Appraisal req)              = do put (0::Word8)
@@ -54,6 +48,151 @@ instance Binary Shared where
                      return (Attestation quote)
              2 -> do res <- get
                      return (Result res)
+
+
+-- Primitive types
+type PCR = Word8
+type Nonce = ByteString
+type Signature = ByteString
+type TPMRequest = Word8 --Request = (Mask, Nonce)
+type Quote = (([PCR], Nonce), Signature)
+
+--Request
+type Request = (DesiredEvidence, TPMRequest, Nonce)
+type DesiredEvidence = [EvidenceDescriptor]
+data EvidenceDescriptor = D0 | D1 | D2  --for now
+
+instance Binary EvidenceDescriptor where
+  put D0 = do put (0::Word8)
+  put D1 = do put (1::Word8)
+  put D2 = do put (2::Word8)
+           
+  get = do t<- get :: Get Word8
+           case t of
+               0 -> return D0
+               1 -> return D1
+               2 -> return D2
+                    
+
+instance Show EvidenceDescriptor where
+  show D0 = "Evidence Piece #0"
+  show D1 = "Evidence Piece #1"
+  show D2 = "Evidence Piece #2"
+   
+
+--Response
+type Response = (EvidencePackage, QuotePackage)
+type EvidencePackage = (Evidence, Nonce, Signature)
+type Hash = ByteString
+type Evidence = [EvidencePiece]
+type EvidencePiece = ByteString --for now 
+type QuotePackage = (Quote, Hash, Signature)
+
+
+prompt:: IO (Int)
+prompt= loop
+      where loop = do putStrLn "Which Domain ID would you like to Appraise?"
+                      input <- getLine
+                      case reads input of
+                        [(id,_)] -> return id
+                        _     -> do putStrLn "Error: Please Enter a Number."
+                                    loop
+
+
+
+main :: IO ()
+main = let (mask, nonce) = mkTPMRequest [0..7] 
+           req = ([D0, D1, D2], mask, nonce) in
+  do chan <- sendRequest req
+     response <- receiveResponse chan
+     --case evaluate req response of True -> putStrLn "Appraisal Succeeded"
+     return ()
+
+                      
+
+-- Appraisal primitives
+
+mkTPMRequest :: [Int] -> (TPMRequest, Nonce)
+mkTPMRequest mask =
+    let mask' = foldr (\ x word -> word `setBit` x) zeroBits mask in
+      (mask', fst $ cprgGenerate 16 gen)
+
+
+sendRequest :: Request -> IO (LibXenVChan)
+sendRequest req = 
+  do id <-getDomId
+     putStrLn $ "Appraiser Domain id: "++(show id)
+     other <- prompt
+     chan <- client_init other
+     putStrLn $ "\n" ++ "Appraiser Sending: "++(show $ Appraisal req) ++ "\n"
+     send chan $ Appraisal req
+     return chan
+
+receiveResponse :: LibXenVChan -> IO Response
+receiveResponse chan =  do
+             ctrlWait chan
+             res :: Shared <- receive chan
+             case res of 
+                                 Attestation response ->  do
+                                                                putStrLn $ "\n" ++ "Appraiser Received: "
+                                                                                               ++ (show res)++ "\n"
+                                                                return response
+                                 otherwise ->  throw $ ErrorCall quoteReceiveError 
+                                                            -- TODO:  error handling here?
+
+{-
+evaluate :: Request -> Quote -> Bool
+evaluate (mask, rnonce) (quote@(qpcrs, qnonce), signature) =
+    let pcrs' = pcrSelect mask in
+        if (not $ verify md5 pub (pack' quote) signature) 
+           then throw $ ErrorCall "Signature could not be verified."
+           else if (rnonce /= qnonce)  
+                then throw $ ErrorCall "Nonce could not be verified."
+                else if (pcrs' /= qpcrs) 
+                     then throw $ ErrorCall "PCR not of expected value."
+                     else True
+-}
+
+--type Quote = (([PCR], Nonce), Signature)
+--type EvidencePackage = (Evidence, Nonce, Signature)
+--type QuotePackage = (Quote, Hash, Signature)
+
+--type Request = (DesiredEvidence, TPMRequest, Nonce)
+--type Response = (EvidencePackage, QuotePackage)
+
+evaluate :: Request -> Response -> Bool
+evaluate (d, tReq, nonce) ((e, nonce', eSig), (((pcrs, nonce''), qSig), hash, qpSig)) = undefined
+
+-- PCR primitives
+pcrs :: [PCR]
+pcrs = correct --wrong
+  where correct :: [PCR]
+        correct = map bit [0..7]
+
+        wrong :: [PCR]
+        --wrong = [(bit 3)] ++ (map bit [1..7])
+        wrong = (map bit [0..6]) ++ [(bit 7)]
+
+pack' :: ([PCR], Nonce) -> ByteString
+pack' (pcrs, nonce) = pack pcrs `append` nonce
+
+pcrSelect :: TPMRequest -> [PCR]
+pcrSelect mask = 
+    [ x | (x, n) <- zip pcrs [0..7], testBit mask n]
+
+-- Crypto primitives
+md5 :: HashDescr
+md5 = hashDescrMD5
+
+gen :: SystemRNG
+gen = unsafePerformIO $ liftM cprgCreate createEntropyPool
+
+pub :: PublicKey
+pri :: PrivateKey
+--(pub, pri) = fromJust $ generateWith (5,11) 255 0x10001
+(pri, pub) = getKeys
+--gen' :: SystemRNG
+--((pub, pri), gen') = generate gen 255 3
 
 
 getKeys :: (PrivateKey, PublicKey)
@@ -78,118 +217,6 @@ readKeys =
 	return (pri, pub)
 
 
-
-
-
-
-
-
-
-
-prompt:: IO (Int)
-prompt= loop
-      where loop = do putStrLn "Which Domain ID would you like to Appraise?"
-                      input <- getLine
-                      case reads input of
-                        [(id,_)] -> return id
-                        _     -> do putStrLn "Error: Please Enter a Number."
-                                    loop
-
-
-
-
-
-
-
-main :: IO ()
-main = let req = mkRequest [0..7] in
-  do chan <- sendRequest req
-     quote <- receiveQuote chan
-     case evaluate req quote of True -> putStrLn "Appraisal Succeeded"
-     return ()
-
-                      
-
-
-   
-                                                                
-
--- Appraisal primitives
-mkRequest :: [Int] -> Request
-mkRequest mask =
-    let mask' = foldr (\ x word -> word `setBit` x) zeroBits mask in
-      (mask', fst $ cprgGenerate 16 gen)
-
-sendRequest :: Request -> IO (LibXenVChan)
-sendRequest req = 
-  do id <-getDomId
-     putStrLn $ "Appraiser Domain id: "++(show id)
-     other <- prompt
-     chan <- client_init other
-     putStrLn $ "\n" ++ "Appraiser Sending: "++(show $ Appraisal req) ++ "\n"
-     send chan $ Appraisal req
-     return chan
-
-receiveQuote :: LibXenVChan -> IO Quote
-receiveQuote chan =  do
-             ctrlWait chan
-             res :: Shared <- receive chan
-             case res of 
-                                 Attestation quote ->  do
-                                                                putStrLn $ "\n" ++ "Appraiser Received: "
-                                                                                               ++ (show res)++ "\n"
-                                                                return quote
-                                 otherwise ->  throw $ ErrorCall quoteReceiveError 
-                                                            -- TODO:  error handling here?
-
-evaluate :: Request -> Quote -> Bool
-evaluate (mask, rnonce) (quote@(qpcrs, qnonce), signature) =
-    let pcrs' = pcrSelect mask in
-        if (not $ verify md5 pub (pack' quote) signature) 
-           then throw $ ErrorCall "Signature could not be verified."
-           else if (rnonce /= qnonce)  
-                then throw $ ErrorCall "Nonce could not be verified."
-                else if (pcrs' /= qpcrs) 
-                     then throw $ ErrorCall "PCR not of expected value."
-                     else True
-
-
-
--- PCR primitives
-pcrs :: [PCR]
-pcrs = correct --wrong
-  where correct :: [PCR]
-        correct = map bit [0..7]
-
-        wrong :: [PCR]
-        --wrong = [(bit 3)] ++ (map bit [1..7])
-        wrong = (map bit [0..6]) ++ [(bit 7)]
-
-pack' :: ([PCR], Nonce) -> ByteString
-pack' (pcrs, nonce) = pack pcrs `append` nonce
-
-pcrSelect :: Mask -> [PCR]
-pcrSelect mask = 
-    [ x | (x, n) <- zip pcrs [0..7], testBit mask n]
-
--- Crypto primitives
-md5 :: HashDescr
-md5 = hashDescrMD5
-
-gen :: SystemRNG
-gen = unsafePerformIO $ liftM cprgCreate createEntropyPool
-
-pub :: PublicKey
-pri :: PrivateKey
---(pub, pri) = fromJust $ generateWith (5,11) 255 0x10001
-(pri, pub) = getKeys
---gen' :: SystemRNG
---((pub, pri), gen') = generate gen 255 3
-
-
-
-
-
---Error messages(for debugging, at least for now)
+--Error messages(only for debugging, at least for now)
 quoteReceiveError :: String
 quoteReceiveError = "Appraiser did not receive a Quote as expected"
--- demos/demo2/Attestation.hs
+++ demos/demo2/Attestation.hs
@@ -52,11 +52,9 @@ instance Binary Shared where
 
 -- Primitive types
 type PCR = Word8
-type Mask = Word8
 type Nonce = ByteString
 type Signature = ByteString
---type Request = (Mask, Nonce)
-type TPMRequest = Word8 --(Mask, Nonce)
+type TPMRequest = Word8 -- Request = (Mask, Nonce)
 type Quote = (([PCR], Nonce), Signature)--simulates TPM 
 
 --Request
@@ -85,7 +83,39 @@ instance Show EvidenceDescriptor where
 --Response
 type Response = (EvidencePackage, QuotePackage)
 type EvidencePackage = (Evidence, Nonce, Signature)
+type Evidence = [EvidencePiece]
+type EvidencePiece = ByteString --for now 
+type Hash = ByteString
 type QuotePackage = (Quote, Hash, Signature)
+
+prompt:: IO (Int)
+prompt= loop
+      where loop = do putStrLn "Which Domain ID is the Appraiser?"
+                      input <- getLine
+                      case reads input of
+                        [(id,_)] -> return id
+                        _     -> do putStrLn "Error: Please Enter a Number."
+                                    loop
+                                    
+                                    
+measurePrompt :: IO (Int)
+measurePrompt = loop
+      where loop = do putStrLn "Which Domain ID is the Measurer?"
+                      input <- getLine
+                      case reads input of
+                        [(id,_)] -> return id
+                        _     -> do putStrLn "Error: Please Enter a Number."
+                                    loop
+
+
+main = do
+  appraiserID <- prompt
+  chan <- server_init appraiserID
+  req <- receiveRequest chan
+  resp <- mkResponse req
+  sendResponse chan resp
+  return ()
+
 
 mkResponse :: Request -> IO Response
 mkResponse (desiredE, desiredPCRs, nonce) = do
@@ -122,26 +152,6 @@ signEvidence e n = case sign Nothing md5 pri res of
    where res = (B.concat e) `append` n
 
 
-
-type Hash = ByteString
-type Evidence = [EvidencePiece]
-type EvidencePiece = ByteString --for now 
-
---Measurement
-type MeasurementRequest = (EvidenceDescriptor, Nonce)
-type MeasurementResponse = (EvidencePiece, Nonce)
-
-
-measurePrompt :: IO (Int)
-measurePrompt = loop
-      where loop = do putStrLn "Which Domain ID is the Measurer?"
-                      input <- getLine
-                      case reads input of
-                        [(id,_)] -> return id
-                        _     -> do putStrLn "Error: Please Enter a Number."
-                                    loop
-
-
 getEvidencePiece :: LibXenVChan -> EvidenceDescriptor -> IO EvidencePiece
 getEvidencePiece chan ed = do
   putStrLn $ "\n" ++ "Attestation Agent Sending: " ++ (show ed)
@@ -152,8 +162,57 @@ getEvidencePiece chan ed = do
   return evidence
   
 
+receiveRequest :: LibXenVChan -> IO Request
+receiveRequest chan = do
+  ctrlWait chan
+  res :: Shared <- receive chan
+  case res of
+    Appraisal req -> do
+      putStrLn $ "\n" ++ "Attester Received: " ++ (show res) ++ "\n"
+      return req
+    otherwise -> throw $ ErrorCall requestReceiveError
+      
+sendResponse :: LibXenVChan -> Response-> IO ()   
+sendResponse chan resp = do
+  putStrLn $ "Attester Sending: " ++ (show $ Attestation resp) ++ "\n"
+  send chan $ Attestation resp
+  return () 
 
+mkSignedQuote :: TPMRequest -> Nonce -> Quote
+mkSignedQuote mask nonce =
+    let pcrs' = pcrSelect mask
+        quote = (pcrs', nonce) in
+      case sign Nothing md5 pri $ pack' quote of
+         Left err -> throw . ErrorCall $ show err
+         Right signature -> (quote, signature) 
+                 
+  -- PCR primitives
+pcrs :: [PCR]
+pcrs = correct --wrong
+  where correct :: [PCR]
+        correct = map bit [0..7]
 
+        wrong :: [PCR]
+        --wrong = [(bit 3)] ++ (map bit [1..7])
+        wrong = (map bit [0..6]) ++ [(bit 7)]
+
+pack' :: ([PCR], Nonce) -> ByteString
+pack' (pcrs, nonce) = pack pcrs `append` nonce
+
+pcrSelect :: TPMRequest -> [PCR]
+pcrSelect mask = 
+    [ x | (x, n) <- zip pcrs [0..7], testBit mask n] 
+
+-- Crypto primitives
+md5 :: HashDescr
+md5 = hashDescrMD5
+
+pub :: PublicKey
+pri :: PrivateKey
+--(pub, pri) = fromJust $ generateWith (5,11) 255 0x10001
+(pri, pub) = getKeys
+--gen' :: SystemRNG
+--((pub, pri), gen') = generate gen 255 3
 
 
 
@@ -177,88 +236,11 @@ readKeys =
             pub = read pubString
         hClose handle
 	return (pri, pub)
-        
-
-prompt:: IO (Int)
-prompt= loop
-      where loop = do putStrLn "Which Domain ID is the Appraiser?"
-                      input <- getLine
-                      case reads input of
-                        [(id,_)] -> return id
-                        _     -> do putStrLn "Error: Please Enter a Number."
-                                    loop
-                                    
-
-receiveRequest :: LibXenVChan -> IO Request
-receiveRequest chan = do
-  ctrlWait chan
-  res :: Shared <- receive chan
-  case res of
-    Appraisal req -> do
-      putStrLn $ "\n" ++ "Attester Received: " ++ (show res) ++ "\n"
-      return req
-    otherwise -> throw $ ErrorCall requestReceiveError
-      
-sendResponse :: LibXenVChan -> Response-> IO ()   
-sendResponse chan resp = do
-  putStrLn $ "Attester Sending: " ++ (show $ Attestation resp) ++ "\n"
-  send chan $ Attestation resp
-  return () 
-
-
-mkSignedQuote :: TPMRequest -> Nonce -> Quote
-mkSignedQuote mask nonce =
-    let pcrs' = pcrSelect mask
-        quote = (pcrs', nonce) in
-      case sign Nothing md5 pri $ pack' quote of
-         Left err -> throw . ErrorCall $ show err
-         Right signature -> (quote, signature) 
-                 
-
-                  
-  -- PCR primitives
-pcrs :: [PCR]
-pcrs = correct --wrong
-  where correct :: [PCR]
-        correct = map bit [0..7]
-
-        wrong :: [PCR]
-        --wrong = [(bit 3)] ++ (map bit [1..7])
-        wrong = (map bit [0..6]) ++ [(bit 7)]
-
-pack' :: ([PCR], Nonce) -> ByteString
-pack' (pcrs, nonce) = pack pcrs `append` nonce
-
-pcrSelect :: Mask -> [PCR]
-pcrSelect mask = 
-    [ x | (x, n) <- zip pcrs [0..7], testBit mask n] 
-
--- Crypto primitives
-md5 :: HashDescr
-md5 = hashDescrMD5
-
-pub :: PublicKey
-pri :: PrivateKey
---(pub, pri) = fromJust $ generateWith (5,11) 255 0x10001
-(pri, pub) = getKeys
---gen' :: SystemRNG
---((pub, pri), gen') = generate gen 255 3
-
-
-
-main = do
-  appraiserID <- prompt
-  chan <- server_init appraiserID
-  req <- receiveRequest chan
-  resp <- mkResponse req
-  sendResponse chan resp
-  return ()
   
   
   
   
-  
-  
+--Error messages(only for debugging, at least for now)
 requestReceiveError :: String
 requestReceiveError = "Attester did not receive a Request as expected"
   
