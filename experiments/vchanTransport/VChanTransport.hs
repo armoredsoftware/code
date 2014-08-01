4dff2fc98b2247bbfe77277523fdb9f2d5d4f9e1
jtdawso <armored@compute2.ext.armored>
2014-08-01 08:12:24 -0500

    removed old transport code

--- experiments/vchanTransport/VChanTransport.hs
+++ experiments/vchanTransport/VChanTransport.hs
@@ -1,41 +1,83 @@
-
+{-# LANGUAGE ScopedTypeVariables #-}
 import Network.Transport
 import VChanUtil
 import qualified Data.ByteString as BS
-mkTransport server id
-	| server = return Transport
+import qualified Data.ByteString.Char8 as BSC
+
+
+
+mkTransport id = return Transport
                           { newEndPoint = serverEnd id
                           , closeTransport = closeT
                           }
-        | not server = return Transport
-                          { newEndPoint = clientInit id
-                          , closeTransport = closeT 
-                          }
 
-clientInit :: Int -> IO(Either (TransportError NewEndPointErrorCode) EndPoint)
-clientInit oid = do vchan <- client_init oid
-                    myId <- getDomId
-                    return $ Right EndPoint{ Network.Transport.receive = undefined 
-                                           , address = EndPointAddress (BS.pack [fromIntegral myId])
-                                           , connect = undefined
-                                           , newMulticastGroup = return $ Left (TransportError NewMulticastGroupUnsupported "Unsupported Method")
-                                           , resolveMulticastGroup = \_-> return $ Left (TransportError ResolveMulticastGroupUnsupported "Unsupported Method")
-                                           , closeEndPoint = undefined
-                                           }
+serverEnd :: Int -> IO(Either (TransportError NewEndPointErrorCode) EndPoint)
+serverEnd oid = do vchan <- server_init oid
+                   myId <- getDomId
 
-serverEnd id = undefined
+                   let myAddress = EndPointAddress (BSC.pack (show myId))
+                       noNewMulticast = return $ Left (TransportError NewMulticastGroupUnsupported "Unsupported Method")
+                       noResolveMulticast= \_-> return $ Left (TransportError ResolveMulticastGroupUnsupported "Unsupported Method")
+
+                   return $ Right EndPoint
+                                 { Network.Transport.receive = receiveData oid vchan
+                                 , address = myAddress
+                                 , connect = createConnection
+                                 , newMulticastGroup = noNewMulticast
+                                 , resolveMulticastGroup = noResolveMulticast
+                                 , closeEndPoint = undefined
+                                 }
 
 closeT  = undefined
+--Unsure about the Connection ID if the client Id is sufficient
+receiveData :: Int -> LibXenVChan -> IO Event
+receiveData clientId chan = do ready<- dataReady chan
+                               if ready < 0 then
+                                    do ctrlWait chan
+                                       receiveData clientId chan
+                               else
+                                 do logger <- createLogger
+                                    str <-readChunkedMessageByteString logger chan 
+                                    return $ Received (fromIntegral clientId) [str]
+
+sendData:: LibXenVChan -> [BS.ByteString] -> IO(Either (TransportError SendErrorCode)())
+sendData chan mesgs = do logger <- createLogger
+                         sequence $ map (\mesg -> sendChunkedMessageByteString logger chan mesg) mesgs
+                         return $ Right () 
+
+createConnection:: EndPointAddress -> Reliability -> ConnectHints-> IO(Either(TransportError ConnectErrorCode) Connection)
+createConnection address _ _ = do 
+           let oid = read $ BSC.unpack (endPointAddressToByteString address)
+           chan <-client_init oid
+	   return $ Right Connection 
+                                   { Network.Transport.send = sendData chan 
+                                   , Network.Transport.close = undefined
+                                   }
+
+prompt:: IO (Int)
+prompt= loop
+      where loop = do putStrLn "Which Domain ID would you like to communicate with?"
+                      input <- getLine
+                      case reads input of
+                         [(id,_)] -> return id
+                         _     -> do putStrLn "Error: Please Enter a Number."
+                                     loop
 
 
-main = do transport <- mkTransport True 5 
+main = do dom <- prompt
+          transport <- mkTransport dom 
           Right endpoint  <- newEndPoint transport
-          Right connection <- connect endpoint (EndPointAddress (BS.pack [1])) ReliableOrdered defaultConnectHints 
-          Network.Transport.send connection [(BS.pack [1..10])]
+         -- Right connection <- connect endpoint (EndPointAddress (BSC.pack (show(dom)) )) ReliableOrdered defaultConnectHints 
+          putStrLn "Sending Message"
+         -- Network.Transport.send connection [(BSC.pack "This will be ignored just signals to create the client init for the testReceive")]
+         -- Network.Transport.send connection [(BSC.pack "This is a test")]
+          
           e <- Network.Transport.receive endpoint
           case e of
-            Received cid mesg -> putStrLn "test"
-            ConnectionClosed cid -> putStrLn "test"
+            Received cid mesg ->  do putStrLn "Received:"
+                                     let strings =  map (\m ->( BSC.unpack m)) mesg
+                                     sequence_ $ map putStrLn strings 
+            ConnectionClosed cid -> putStrLn "closed"
             ConnectionOpened cid rel addr -> putStrLn (show addr) 
             
           putStrLn ""
