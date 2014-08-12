54d373b112fb700f110cc479e9a921dae4a840af
Paul Kline <pauliankline@gmail.com>
2014-08-12 13:09:23 -0500

    Corrected jsonEncode for EvidencePiece to account for the different rep types

--- experiments/javaJsonStuff/codepackage/EvidencePiece.java
+++ experiments/javaJsonStuff/codepackage/EvidencePiece.java
@@ -5,58 +5,67 @@ import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-public class EvidencePiece{
-   	private long[] m0Rep;
-   	private String tag;
+public class EvidencePiece {
+	private long[] mRep;
+	private String tag;
+	private String mReptag;
 
- 	public EvidencePiece(JSONObject jObj) {
-		setTag((String)(jObj.get("tag")));
-
-		List<Long> lst = (List<Long>)jObj.get("m0Rep_EvidencePiece");
-		if (lst == null) {
-			lst = (List)jObj.get("m0Rep");
+	public EvidencePiece(JSONObject jObj) {
+		setTag((String) (jObj.get("tag")));
+		List<Long> lst=null;
+		if (tag.compareTo("M0") == 0) {
+			mReptag="m0Rep_EvidencePiece";
+			
+		}else if (tag.compareTo("M1") == 0) {
+			mReptag="m1Rep_EvidencePiece";
+		}else if (tag.compareTo("M2") == 0) {
+			mReptag="m2Rep_EvidencePiece";
 		}
-		m0Rep = new long[lst.size()];
-		for (int i = 0; i < lst.size(); i++) {
-			m0Rep[i] = (long) lst.get(i);
-		}
-		//setM0Rep();
 		
+		lst = (List<Long>) jObj.get(mReptag);
+		mRep = new long[lst.size()];
+		for (int i = 0; i < lst.size(); i++) {
+			mRep[i] = (long) lst.get(i);
+		}
+		// setM0Rep();
+
 	}
- 	public EvidencePiece(long[] data, String constructor){
- 		m0Rep=data;
- 		tag=constructor;
- 		
- 	}
-	public long[] getM0Rep(){
-		return this.m0Rep;
+
+	public EvidencePiece(long[] data, String constructor) {
+		mRep = data;
+		tag = constructor;
+
 	}
-	public void setM0Rep(long[] m0Rep){
-		this.m0Rep = m0Rep;
+
+	public long[] getM0Rep() {
+		return this.mRep;
 	}
- 	public String getTag(){
+
+	public void setM0Rep(long[] m0Rep) {
+		this.mRep = m0Rep;
+	}
+
+	public String getTag() {
 		return this.tag;
 	}
-	public void setTag(String tag){
+
+	public void setTag(String tag) {
 		this.tag = tag;
 	}
-	
-	public JSONObject jsonEncode(){
+
+	public JSONObject jsonEncode() {
 		JSONObject jObj = new JSONObject();
 		jObj.put("tag", tag);
 		JSONArray jarray = new JSONArray();
-		for (int i = 0; i < m0Rep.length; i++) {
-			jarray.add(m0Rep[i]);
+		for (int i = 0; i < mRep.length; i++) {
+			jarray.add(mRep[i]);
 		}
 		
-		jObj.put("m0Rep_EvidencePiece",jarray);
+		jObj.put(mReptag, jarray);
 		JSONObject wrapper = new JSONObject();
 		wrapper.put("tag", "EvidencePieceW");
 		wrapper.put("getEvidencePiece", jObj);
 		return wrapper;
-		
-		
-		
-		
+
 	}
 }
